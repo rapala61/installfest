d4ee05c441389c8f86d2a79831ae9fc706a5a271
Philip Hughes <philip@h4w5.com>
2015-04-14 17:34:41 -0400

    update app list for ubuntu

--- scripts/ubuntu/install_apps.sh
+++ scripts/ubuntu/install_apps.sh
@@ -9,10 +9,10 @@ sudo apt-get -y update
 sudo apt-get -y install google-chrome-stable
 
 # hipchat
-sudo sh -c 'echo "deb http://downloads.hipchat.com/linux/apt stable main" > /etc/apt/sources.list.d/atlassian-hipchat.list'
-wget -O - https://www.hipchat.com/keys/hipchat-linux.key | sudo apt-key add -
-sudo apt-get -y update
-sudo apt-get -y install hipchat
+# sudo sh -c 'echo "deb http://downloads.hipchat.com/linux/apt stable main" > /etc/apt/sources.list.d/atlassian-hipchat.list'
+# wget -O - https://www.hipchat.com/keys/hipchat-linux.key | sudo apt-key add -
+# sudo apt-get -y update
+# sudo apt-get -y install hipchat
 
 # sublime
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
@@ -20,12 +20,12 @@ sudo apt-get -y update
 sudo apt-get -y install sublime-text
 
 # zsh
-sudo apt-get -y install zsh
+# sudo apt-get -y install zsh
 
 # flux
-sudo add-apt-repository -y ppa:kilian/f.lux
-sudo apt-get -y update
-sudo apt-get -y install fluxgui
+# sudo add-apt-repository -y ppa:kilian/f.lux
+# sudo apt-get -y update
+# sudo apt-get -y install fluxgui
 
 # hub
 git clone https://github.com/github/hub.git
@@ -37,5 +37,4 @@ rake install prefix=/usr/local
 sudo apt-get -y install vim
 
 # emacs
-
-sudo apt-get -y install emacs
+# sudo apt-get -y install emacs
