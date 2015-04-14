ae5a6098bf4d0b148a8cecec847fe11ca404d0e6
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2015-04-14 15:41:23 -0400

    updates to use iojs

--- scripts/install_node_and_npm.sh
+++ scripts/install_node_and_npm.sh
@@ -4,5 +4,9 @@ nvm install stable
 # Install iojs
 nvm install iojs
 
+# Use iojs
+# (phlco) could set this with ~/.nvmrc
+nvm use iojs
+
 # Ensure we have the most recent version of npm
 npm install npm -g
