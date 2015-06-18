1ef53055082d6491b01520d9577ffbe227462652
h4w5 <philip@h4w5.com>
2015-06-18 15:19:00 -0700

    add git shortcuts

--- scripts/configure_git.sh
+++ scripts/configure_git.sh
@@ -24,3 +24,13 @@ git config --global push.default current
 
 # set global gitignore
 git config --global core.excludesfile ~/.gitignore_global
+
+# add some useful shortcuts
+git config --global alias.sha 'rev-parse HEAD'
+git config --global alias.last 'log -1 HEAD --pretty=oneline'
+git config --global alias.lastid 'rev-parse --short HEAD'
+git config --global alias.shalog 'log --pretty=oneline'
+git config --global alias.set-master 'branch --set-upstream-to=origin/master master'
+git config --global alias.back 'reset --soft HEAD~1'
+git config --global alias.again 'commit -c ORIG_HEAD'
+git config --global alias.deploy 'subtree push --prefix dist/ origin gh-pages'
