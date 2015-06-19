46f5f37aa906d54c54fdc24cadd392fee59f705d
h4w5 <philip@h4w5.com>
2015-06-19 11:54:03 -0700

    Rebuilds scripts

--- scripts/install_node_and_npm.sh
+++ scripts/install_node_and_npm.sh
@@ -9,7 +9,7 @@ nvm install iojs-v1.6.2
 
 # Use iojs
 # (phlco) could set this with ~/.nvmrc
-nvm use iojs-v.1.6.2
+nvm use iojs-v1.6.2
 
 # Ensure we have the most recent version of npm
 npm install npm -g
