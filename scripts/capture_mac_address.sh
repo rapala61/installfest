5908f6ad34c17947afe889801d3bd7d546b47c15
Dennis Liaw <dennis.liaw@gmail.com>
2015-08-03 14:34:48 -0400

    adds changes from master

--- scripts/capture_mac_address.sh
+++ scripts/capture_mac_address.sh
@@ -3,4 +3,5 @@ echo "attendance purposes"
 
 # Get MAC address for attendance
 # TODO (phlco) do we need sudo here again?
+#TODO (CentroDL) hook this up to meraki for wdi pluto
 mac_address=$(sudo ifconfig en0 | grep ether | sed -e 's/^[ \t|ether|\s|\n]*//')
