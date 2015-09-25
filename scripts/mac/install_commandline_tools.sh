fc1521d71ca80641b941a18bc593e61c9e297ac3
hews <p@hews.co>
2015-09-25 08:46:01 -0700

    Add 10.11 to commandline tool check and clean up the script

--- scripts/mac/install_commandline_tools.sh
+++ scripts/mac/install_commandline_tools.sh
@@ -1,6 +1,9 @@
 
+# mac/install_commandline_tools.sh
+
 # Check that command line tools are installed
 case $os_version in
+  *10.11*) cmdline_version="CLTools_Executables" ;; # El Capitan
   *10.10*) cmdline_version="CLTools_Executables" ;; # Yosemite
   *10.9*)  cmdline_version="CLTools_Executables" ;; # Mavericks
   *10.8*)  cmdline_version="DeveloperToolsCLI"   ;; # Mountain Lion
@@ -15,26 +18,23 @@ esac
 if [ ! -z $(pkgutil --pkgs=com.apple.pkg.$cmdline_version) ]; then
   inform "Command Line Tools are installed!";
 elif [[ $os_version == *10.6** ]]; then
-  warn "Command Line Tools are not installed"
-  warn "Downloading and installing the GCC compiler."
-  warn "When you're done rerun this script"
+  warn "Command Line Tools are not installed!"
+  warn "  Downloading and installing the GCC compiler."
+  warn "  When you're done rerun this script..."
   curl -OLk https://github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.6.pkg
   open GCC-10.6.pkg
   exit 1
 elif [[ $os_version == *10.7* ]] || [[ $os_version == *10.8* ]]; then
-  warn "Command Line Tools are not installed"
+  warn "Command Line Tools are not installed!"
   warn "Register for a Developer Account"
-  warn "Download the Command Lion Tools from"
-  warn "https://developer.apple.com/downloads/index.action"
-  warn "Then rerun this script"
-  exit 1 # TODO (phlco) maybe just type 'done' instead of rerunning?
+  warn "  Download the Command Lion Tools from:"
+  warn "    https://developer.apple.com/downloads/index.action"
+  warn "  and then rerun this script..."
+  exit 1
 else
   warn "Command Line Tools are not installed!"
-  # echo "run '$ sudo xcodebuild -license' then"
-  # echo "'$ xcode-select --install'"
-  # echo "Then rerun this script."
-  warn "Running 'xcode-select --install' Please click continue!"
-  warn "After installing please rerun the installfest script."
+  warn "  Running 'xcode-select --install' Please click continue!"
+  warn "  After installing please rerun this script."
   xcode-select --install
-  exit 1 # TODO (phlco) maybe just type 'done' instead of rerunning?
+  exit 1
 fi
