60b94499ab6c7f8f41be56fc1629c825a43f5819
hews <p@hews.co>
2015-09-25 10:45:16 -0700

    Update Node/NVM to use stable Node now that Iojs is merged.

--- scripts/install_node_and_npm.sh
+++ scripts/install_node_and_npm.sh
@@ -1,15 +1,9 @@
 # load nvm command
 source ~/.nvm/nvm.sh
 
-# Install latest version of Node
+# Install and use the latest version of Node
 nvm install stable
-
-# Install iojs
-nvm install iojs-v1.6.2
-
-# Use iojs
-# (phlco) could set this with ~/.nvmrc
-nvm use iojs-v1.6.2
+nvm use stable
 
 # Ensure we have the most recent version of npm
 npm install npm -g
