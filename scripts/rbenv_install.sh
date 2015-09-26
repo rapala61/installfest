bbdd5135e34bbfceb39707d0150911df6ea24bf2
hews <p@hews.co>
2015-09-25 17:48:26 -0700

    updates to rbenv install, move it from mac to main

--- scripts/rbenv_install.sh
+++ scripts/rbenv_install.sh
@@ -7,9 +7,9 @@ git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 # export RBENV_ROOT="/usr/local/var/rbenv"
 
 # Add to bash_profile
-echo '# added by installfest script' >> ~/.bash_profile
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
+# echo '# added by installfest script' >> ~/.bash_profile
+# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+# echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
 
 # enable shims and autocompletion
 export PATH="$HOME/.rbenv/bin:$PATH"
@@ -21,6 +21,9 @@ git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/r
 # Automatically install gems every time you install a new version of Ruby
 # brew install rbenv-default-gems
 git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
+
+# automatically runs rbenv rehash every time you install or uninstall a gem
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
 
 # # Provides an `rbenv install` command
 # # ruby-build is a dependency of rbenv-default-gems, so it gets installed
