2d3180df219381be4a7fb4d54835c68df9a319ee
hews <p@hews.co>
2015-10-06 16:23:15 -0700

    update again to fix rbenv build

--- scripts/rbenv_install.sh
+++ scripts/rbenv_install.sh
@@ -22,20 +22,20 @@ inform "Installing rbenv plugins..." true
 
 RBENV_REHASH="${RBENV_DIR}/plugins/rbenv-gem-rehash"
 RBENV_DEFAULT="${RBENV_DIR}/plugins/rbenv-default-gems"
-RBENV_BUILD="${RBENV_DIR}/plugins/rbenv-gem-rehash"
+RBENV_BUILD="${RBENV_DIR}/plugins/ruby-build"
 
 if [[ -e "$RBENV_REHASH" ]]; then
   show "Rehash plugin already installed! Moving on..."
 else
   # Automatically install gems every time you install a new version of Ruby
-  git clone https://github.com/sstephenson/rbenv-default-gems.git "$RBENV_REHASH"
+  git clone https://github.com/sstephenson/rbenv-gem-rehash.git "$RBENV_REHASH"
 fi
 
 if [[ -e "$RBENV_DEFAULT" ]]; then
   show "Default gems plugin installed! Moving on..."
 else
   # Automatically runs rbenv rehash every time you install or uninstall a gem
-  git clone https://github.com/sstephenson/rbenv-gem-rehash.git "$RBENV_DEFAULT"
+  git clone https://github.com/sstephenson/rbenv-default-gems.git "$RBENV_DEFAULT"
 fi
 
 if [[ -e "$RBENV_BUILD" ]]; then
