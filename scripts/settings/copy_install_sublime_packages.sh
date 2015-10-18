53594751407554e887471c745ef6d0667f0df897
hews <p@hews.co>
2015-10-18 00:39:25 -0700

    fix mkdir for sublime

--- scripts/settings/copy_install_sublime_packages.sh
+++ scripts/settings/copy_install_sublime_packages.sh
@@ -6,7 +6,7 @@ SUBLIME_DIR="${HOME}/Library/Application Support/Sublime Text 3"
 SUBLIME_PACKAGES_DIR="${SUBLIME_DIR}/Installed Packages"
 
 # ensure files exist before copying
-sudo mkdir -p "SUBLIME_PACKAGES_DIR"
+mkdir -p "$SUBLIME_PACKAGES_DIR"
 
 inform "Copying Sublime packages..." true
 copy_files subl_packages "$SUBLIME_PACKAGES_DIR" "$SCRIPT_SUBL_PACKAGES"
