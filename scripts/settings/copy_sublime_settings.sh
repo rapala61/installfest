2bd4207add2750ee080332113f6366d05e615a1d
hews <p@hews.co>
2015-10-18 00:35:43 -0700

    add sudo to mkdir for sublime

--- scripts/settings/copy_sublime_settings.sh
+++ scripts/settings/copy_sublime_settings.sh
@@ -6,7 +6,7 @@ SUBLIME_DIR="${HOME}/Library/Application Support/Sublime Text 3"
 SUBLIME_SETTINGS_DIR="${SUBLIME_DIR}/Packages/User"
 
 # ensure files exist before copying
-mkdir -p "SUBLIME_SETTINGS_DIR"
+sudo mkdir -p "SUBLIME_SETTINGS_DIR"
 
 inform "Copying Sublime settings..." true
 copy_files subl_settings "$SUBLIME_SETTINGS_DIR" "$SCRIPT_SUBL_SETTINGS"
