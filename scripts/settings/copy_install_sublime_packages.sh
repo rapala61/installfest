2d67ec9d1b4a5bf185f02232a5f67dd2fd57e878
hews <p@hews.co>
2015-10-19 11:54:31 -0700

    fixes to installing sublime package control
    Closes #102, closes #96, closes #52.

--- scripts/settings/copy_install_sublime_packages.sh
+++ scripts/settings/copy_install_sublime_packages.sh
@@ -4,12 +4,14 @@
 
 SUBLIME_DIR="${HOME}/Library/Application Support/Sublime Text 3"
 SUBLIME_PACKAGES_DIR="${SUBLIME_DIR}/Installed Packages"
+SUBLIME_SETTINGS_DIR="${SUBLIME_DIR}/Packages/User"
 
 # ensure files exist before copying
 mkdir -p "$SUBLIME_PACKAGES_DIR"
+mkdir -p "$SUBLIME_SETTINGS_DIR"
 
 inform "Copying Sublime packages..." true
-copy_files subl_packages "$SUBLIME_PACKAGES_DIR" "$SCRIPT_SUBL_PACKAGES"
+copy_files subl_packages "$SUBLIME_SETTINGS_DIR" "$SCRIPT_SUBL_PACKAGES"
 show "Complete!"
 
 # TODO (pj) get Package Control up and running with settings, etc.
@@ -25,5 +27,5 @@ PKG_CNTRL_FILE="Package Control.sublime-package"
 # mv "$SRC_DIR/../packages" "$SUBLIME_PACKAGES_DIR/$PKG_CNTRL_FILE"
 
 inform "Downloading Sublime Package Control..." true
-curl -o "${SUBLIME_PACKAGES_DIR}/$PKG_CNTRL_FILE" PKG_CNTRL_URI
+curl -o "${SUBLIME_PACKAGES_DIR}/$PKG_CNTRL_FILE" $PKG_CNTRL_URI
 show "Complete!"
